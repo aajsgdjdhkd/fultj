ed1a58d0c124e56369f758229abdf3b8d64910ef
aajsgdjdhkd <1925154989@qq.com>
2026-04-27 00:01:07 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const net = require('net');
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 8080;
+const server = new WebSocket.Server({ port: port });
 
 let clients = [];
 let clientCount = 0;
 
-const server = net.createServer((socket) => {
+console.log(`聊天室服务端已启动，端口: ${port}`);
+
+server.on('connection', function connection(ws) {
     clientCount++;
     const clientId = clientCount;
     const clientName = '用户' + clientId;
@@ -11,56 +16,51 @@ const server = net.createServer((socket) => {
     clients.push({
         id: clientId,
         name: clientName,
-        socket: socket
+        ws: ws
     });
     
-    console.log(`${clientName} 已连接，当前在线: ${clients.length}`);
-    
-    //广播加入消息
-    broadcast(`【系统】${clientName} 加入了聊天室`);
+    //广播新用户加入
+    broadcast('【系统】' + clientName + ' 加入了聊天室', null);
     broadcastOnlineCount();
     
-    socket.on('data', (data) => {
-        const message = data.toString().trim();
-        console.log(`${clientName}: ${message}`);
-        broadcast(`${clientName}: ${message}`, socket);
+    console.log(clientName + '已连接，当前在线: ' + clients.length);
+    
+    ws.on('message', function message(data) {
+        const msg = data.toString();
+        console.log(clientName + ': ' + msg);
+        broadcast(clientName + ': ' + msg, ws);
     });
     
-    socket.on('close', () => {
-        clients = clients.filter(c => c.socket !== socket);
-        broadcast(`【系统】${clientName} 离开了聊天室`);
+    ws.on('close', function close() {
+        clients = clients.filter(function(c) {
+            return c.ws !== ws;
+        });
+        
+        broadcast('【系统】' + clientName + ' 离开了聊天室', null);
         broadcastOnlineCount();
-        console.log(`${clientName} 已断开，当前在线: ${clients.length}`);
+        console.log(clientName + '已断开，当前在线: ' + clients.length);
     });
     
-    socket.on('error', (err) => {
-        console.log(`错误: ${err.message}`);
+    ws.on('error', function error(err) {
+        console.log('错误: ' + err.message);
     });
 });
 
-function broadcast(message, senderSocket = null) {
-    clients.forEach(client => {
-        if (client.socket !== senderSocket) {
-            client.socket.write(message + '\n');
+function broadcast(message, senderWs) {
+    clients.forEach(function(client) {
+        if (client.ws !== senderWs) {
+            if (client.ws.readyState === WebSocket.OPEN) {
+                client.ws.send(message);
+            }
         }
     });
 }
 
 function broadcastOnlineCount() {
-    broadcast(`【系统】当前在线人数: ${clients.length}`);
-}
-
-server.listen(8888, '0.0.0.0', () => {
-    console.log('聊天室服务端已启动，端口: 8888');
-    console.log('本机IP地址:');
-    
-    const os = require('os');
-    const interfaces = os.networkInterfaces();
-    for (let name in interfaces) {
-        for (let iface of interfaces[name]) {
-            if (iface.family === 'IPv4' && !iface.internal) {
-                console.log(`  http://${iface.address}:8888`);
-            }
+    const msg = '【系统】当前在线人数: ' + clients.length;
+    clients.forEach(function(client) {
+        if (client.ws.readyState === WebSocket.OPEN) {
+            client.ws.send(msg);
         }
-    }
-});
+    });
+}
